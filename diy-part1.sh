eb25e9efedf4637d19e309fbea109772d3b7f7c7
umbei <50173839+umbei@users.noreply.github.com>
2023-03-04 21:43:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,138 @@
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+# 修改默认IP
+sed -i 's/192.168.1.1/10.0.0.252/g' package/base-files/files/bin/config_generate
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# Change default shell to zsh
+# sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# Clear the login password
+# sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+
+# 移除重复软件包
+# rm -rf feeds/packages/net/mosdns
+# rm -rf feeds/luci/themes/luci-theme-argon
+# rm -rf feeds/luci/themes/luci-theme-netgear
+# rm -rf feeds/luci/applications/luci-app-netdata
+# rm -rf feeds/luci/applications/luci-app-wrtbwmon
+# rm -rf feeds/luci/applications/luci-app-dockerman
+
+# 添加额外软件包
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+# echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+# git clone --depth 1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+# git clone --depth 1 https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+# git clone --depth 1 https://github.com/iwrt/luci-app-ikoolproxy package/luci-app-ikoolproxy
+# git clone --depth 1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+git clone --depth 1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+# git clone --depth 1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
+# git clone --depth 1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+# svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
+# svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman package/luci-app-dockerman
+# svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/luci-app-eqos package/luci-app-eqos
+
+#添加smartdns
+git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+
+# 科学上网插件
+# git clone --depth 1 https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
+# git clone --depth 1 https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
+# svn co https://github.com/kiddin9/openwrt-bypass/trunk/luci-app-bypass package/luci-app-bypass
+# svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+# svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
+# svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
+# svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+
+# 科学上网插件依赖
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook package/brook
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng package/chinadns-ng
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/dns2socks package/dns2socks
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/dns2tcp package/dns2tcp
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/hysteria package/hysteria
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ipt2socks package/ipt2socks
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/microsocks package/microsocks
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/naiveproxy package/naiveproxy
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/pdnsd-alt package/pdnsd-alt
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/sagernet-core package/sagernet-core
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks package/ssocks
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/tcping package/tcping
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/trojan-go
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus package/trojan-plus
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-geodata package/v2ray-geodata
+# svn co https://github.com/fw876/helloworld/trunk/simple-obfs package/simple-obfs
+# svn co https://github.com/fw876/helloworld/trunk/v2ray-core package/v2ray-core
+# svn co https://github.com/fw876/helloworld/trunk/v2ray-plugin package/v2ray-plugin
+# svn co https://github.com/fw876/helloworld/trunk/shadowsocks-rust package/shadowsocks-rust
+# svn co https://github.com/fw876/helloworld/trunk/shadowsocksr-libev package/shadowsocksr-libev
+# svn co https://github.com/fw876/helloworld/trunk/xray-core package/xray-core
+# svn co https://github.com/fw876/helloworld/trunk/xray-plugin package/xray-plugin
+# svn co https://github.com/fw876/helloworld/trunk/lua-neturl package/lua-neturl
+# svn co https://github.com/fw876/helloworld/trunk/trojan package/trojan
+# svn co https://github.com/fw876/helloworld/trunk/redsocks2 package/redsocks2
+
+# Themes
+# git clone --depth 1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+# git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+# git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+# git clone --depth 1 https://github.com/thinktip/luci-theme-neobird package/luci-theme-neobird
+# git clone --depth 1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
+svn co https://github.com/rosywrt/luci-theme-rosy/trunk/luci-theme-rosy package/luci-theme-rosy
+# svn co https://github.com/haiibo/packages/trunk/luci-theme-atmaterial package/luci-theme-atmaterial
+# svn co https://github.com/haiibo/packages/trunk/luci-theme-opentomcat package/luci-theme-opentomcat
+# svn co https://github.com/haiibo/packages/trunk/luci-theme-netgear package/luci-theme-netgear
+
+
+# MosDNS
+# svn co https://github.com/sbwml/luci-app-mosdns/trunk/luci-app-mosdns package/luci-app-mosdns
+# svn co https://github.com/sbwml/luci-app-mosdns/trunk/mosdns package/mosdns
+
+
+# 流量监控
+# svn co https://github.com/haiibo/packages/trunk/luci-app-wrtbwmon package/luci-app-wrtbwmon
+# svn co https://github.com/haiibo/packages/trunk/wrtbwmon package/wrtbwmon
+
+# Alist
+# svn co https://github.com/sbwml/luci-app-alist/trunk/luci-app-alist package/luci-app-alist
+# svn co https://github.com/sbwml/luci-app-alist/trunk/alist package/alist
+
+# iStore
+# svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
+# svn co https://github.com/linkease/istore/trunk/luci package/istore
+
+# 设置向导
+# svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-wizard package/luci-app-wizard
+# sed -i 's/"admin"/"admin", "system"/g' package/luci-app-wizard/luasrc/controller/wizard.lua
+
+# 在线用户
+# svn co https://github.com/haiibo/packages/trunk/luci-app-onliner package/luci-app-onliner
+# sed -i '/bin\/sh/a\uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean/default-settings/files/zzz-default-settings
+# sed -i '/nlbwmon/a\uci commit nlbwmon' package/lean/default-settings/files/zzz-default-settings
+
+# 修改版本为编译日期
+# date_version=$(date +"%Y.%m.%d")
+# orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+# sed -i "s/${orig_version}/R${date_version} by umbei/g" package/lean/default-settings/files/zzz-default-settings
+
+# 调整 x86 型号只显示 CPU 型号
+sed -i '/h=${g}.*/d' package/lean/autocore/files/x86/autocore
+sed -i 's/(dmesg.*/{a}${b}${c}${d}${e}${f}/g' package/lean/autocore/files/x86/autocore
+sed -i 's/echo $h/echo $g/g' package/lean/autocore/files/x86/autocore
+
+# 修改 Makefile
+# find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\.\/\.\.\/luci\.mk/include \$(TOPDIR)\/feeds\/luci\/luci\.mk/g' {}
+# find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/include\ \.\.\/\.\.\/lang\/golang\/golang\-package\.mk/include \$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang\-package\.mk/g' {}
+# find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=\@GHREPO/PKG_SOURCE_URL:=https:\/\/github\.com/g' {}
+# find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=\@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload\.github\.com/g' {}
+
+# 删除主题强制默认
+find package/luci-theme*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
+
+# 调整 V2ray服务器 到 VPN 菜单
+# sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/controller/*.lua
+# sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/model/cbi/v2ray_server/*.lua
+# sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/view/v2ray_server/*.htm
+
+./scripts/feeds update -a
+./scripts/feeds install -a
