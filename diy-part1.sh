1f4d8c9c421185697579045d5155d30dba68ae3b
umbei <50173839+umbei@users.noreply.github.com>
2023-03-08 21:09:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,10 +18,9 @@ sed -i 's/root:::0:99999:7:::/root:$2y$10$Czru1JJHmsJlSTjbWROSX.GvSqHXELlmiPqsCb
 # rm -rf feeds/luci/applications/luci-app-dockerman
 
 # 添加额外软件包
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+echo 'src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default
 # echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 git clone --depth 1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 # git clone --depth 1 https://github.com/iwrt/luci-app-ikoolproxy package/luci-app-ikoolproxy
