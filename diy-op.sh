33ea2ba92261720e88702311ca74d3530a7caacf
umbei <50173839+umbei@users.noreply.github.com>
2023-03-16 21:11:42 +0800

    Update diy-op.sh

--- diy-op.sh
+++ diy-op.sh
@@ -11,4 +11,16 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/10.0.0.251/10.0.0.101/g' package/base-files/files/bin/config_generate
+
+# 添加额外软件包
+echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+# git clone --depth 1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+# git clone --depth 1 https://github.com/iwrt/luci-app-ikoolproxy package/luci-app-ikoolproxy
+# git clone --depth 1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+# git clone --depth 1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-adguardhome package/luci-app-adguardhome
