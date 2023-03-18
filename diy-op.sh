fcecdf8b93884607dcde37d5e0b7ff0e6eb76d7a
umbei <50173839+umbei@users.noreply.github.com>
2023-03-18 16:00:06 +0800

    Update diy-op.sh

--- diy-op.sh
+++ diy-op.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/10.0.0.251/10.0.0.101/g' package/base-files/files/bin/config_generate
+# sed -i 's/10.0.0.251/10.0.0.101/g' package/base-files/files/bin/config_generate
 
 # 添加额外软件包
 echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
@@ -22,7 +22,7 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.defa
 # git clone --depth 1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 # git clone --depth 1 https://github.com/iwrt/luci-app-ikoolproxy package/luci-app-ikoolproxy
 # git clone --depth 1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-adguardhome package/luci-app-adguardhome
+# svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-adguardhome package/luci-app-adguardhome
 
 #添加smartdns
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
