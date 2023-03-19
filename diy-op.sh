43deb6f0f2275cd82313ec52bd8cddc7bc00a1db
umbei <50173839+umbei@users.noreply.github.com>
2023-03-19 17:17:14 +0800

    Update diy-op.sh

--- diy-op.sh
+++ diy-op.sh
@@ -1,8 +1,9 @@
 #
 # 添加额外软件包
 echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 # git clone --depth 1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+echo 'src-git wall https://github.com/kenzok8/wall' >>feeds.conf.default
