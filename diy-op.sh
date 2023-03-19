edc3875d9bacdba9e37d92b835f0274374f04b1b
umbei <50173839+umbei@users.noreply.github.com>
2023-03-19 20:30:37 +0800

    Update diy-op.sh

--- diy-op.sh
+++ diy-op.sh
@@ -1,6 +1,7 @@
 #
 # 添加额外软件包
 echo 'src-git Clash https://github.com/Dreamacro/clash' >>feeds.conf.default
+echo 'src-git Clash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
