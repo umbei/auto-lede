406e97cb7ae2cc8191019102ce9618af2e800a84
umbei <50173839+umbei@users.noreply.github.com>
2023-03-19 20:41:43 +0800

    Update diy-op.sh

--- diy-op.sh
+++ diy-op.sh
@@ -1,32 +1,31 @@
 ## 修改默认IP
-sed -i 's/192.168.1.1/10.0.0.252/g' package/base-files/files/bin/config_generate
+sed -i 's/10.0.0.251/10.0.0.101/g' package/base-files/files/bin/config_generate
 
 # Change default shell to zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # Clear the login password
-sed -i "$2y$10$Czru1JJHmsJlSTjbWROSX.GvSqHXELlmiPqsCb15//ALXYeJ4VN76" package/lean/default-settings/files/zzz-default-settings
+# sed -i "$2y$10$Czru1JJHmsJlSTjbWROSX.GvSqHXELlmiPqsCb15//ALXYeJ4VN76" package/lean/default-settings/files/zzz-default-settings
 
 
 # 添加额外软件包
 echo 'src-git Clash https://github.com/Dreamacro/clash' >>feeds.conf.default
-echo 'src-git Clash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git wall https://github.com/kenzok8/wall' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
-echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-# git clone --depth 1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-echo 'src-git wall https://github.com/kenzok8/wall' >>feeds.conf.default
 
 # 修改版本为编译日期
-date_version=$(date +"%Y.%m.%d")
-orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-sed -i "s/${orig_version}/R${date_version} by umbei/g" package/lean/default-settings/files/zzz-default-settings
+# date_version=$(date +"%Y.%m.%d")
+# orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+# sed -i "s/${orig_version}/R${date_version} by umbei/g" package/lean/default-settings/files/zzz-default-settings
 
 # 调整 x86 型号只显示 CPU 型号
-sed -i '/h=${g}.*/d' package/lean/autocore/files/x86/autocore
-sed -i 's/(dmesg.*/{a}${b}${c}${d}${e}${f}/g' package/lean/autocore/files/x86/autocore
-sed -i 's/echo $h/echo $g/g' package/lean/autocore/files/x86/autocore
+# sed -i '/h=${g}.*/d' package/lean/autocore/files/x86/autocore
+# sed -i 's/(dmesg.*/{a}${b}${c}${d}${e}${f}/g' package/lean/autocore/files/x86/autocore
+# sed -i 's/echo $h/echo $g/g' package/lean/autocore/files/x86/autocore
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
