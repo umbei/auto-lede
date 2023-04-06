0d4b95f11238b59687403abdda6263ccc58c9d66
umbei <50173839+umbei@users.noreply.github.com>
2023-04-06 21:02:47 +0800

    Update diy-op.sh

--- diy-op.sh
+++ diy-op.sh
@@ -14,15 +14,16 @@ sed -i 's/10.0.0.251/10.0.0.101/g' package/base-files/files/bin/config_generate
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/umbei/passwall' >> feeds.conf.default
-svn co https://github.com/rosywrt/luci-theme-rosy/trunk/luci-theme-rosy package/luci-theme-rosy
-svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-adguardhome package/luci-app-adguardhome
-svn co https://github.com/kenzok8/openwrt-packages/tree/master/adguardhome package/adguardhome
-svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-ikoolproxy package/luci-app-ikoolproxy
-svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-aliddns package/luci-app-aliddns
-svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-passwall package/luci-app-passwall
-svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-passwall2 package/luci-app-passwall2
-git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
+# svn co https://github.com/rosywrt/luci-theme-rosy/trunk/luci-theme-rosy package/luci-theme-rosy
+# svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-adguardhome package/luci-app-adguardhome
+# svn co https://github.com/kenzok8/openwrt-packages/tree/master/adguardhome package/adguardhome
+git clone --depth 1 https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-ikoolproxy package/luci-app-ikoolproxy
+git clone --depth 1 https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-aliddns package/luci-app-aliddns
+# git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
 git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+git clone --depth 1 https://github.com/iwrt/luci-app-ikoolproxy package/luci-app-ikoolproxy
+# git clone --depth 1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+
 
 # 梯子依赖插件
 echo 'src-git passwall https://github.com/umbei/passwall' >> feeds.conf.default
