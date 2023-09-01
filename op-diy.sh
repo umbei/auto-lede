039deb111a77b4c111d1a4226cec592df0af7ccd
umbei <50173839+umbei@users.noreply.github.com>
2023-09-01 21:48:30 +0800

    Create op-diy.sh

---
+++ op-diy.sh
@@ -0,0 +1,6 @@
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+git pull
+./scripts/feeds update -a
+./scripts/feeds install -a
+make menuconfig
