6b3deb2934de54ed89c5e9f25a2c703fd196cec7
umbei <50173839+umbei@users.noreply.github.com>
2023-09-01 21:49:22 +0800

    Update op-diy.sh

--- op-diy.sh
+++ op-diy.sh
@@ -1,3 +1,12 @@
+# 修改默认IP
+sed -i 's/10.0.0.253/10.0.0.252/g' package/base-files/files/bin/config_generate
+
+# Change default shell to zsh
+# sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+
+# Clear the login password
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 git pull
